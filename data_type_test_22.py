ab3794f57e1f43d3aa95de24ba047f4cc6699f30
sanjayprakash7113 <sanjayprakash15112@gmail.com>
2026-03-09 21:44:10 +0530

    Add files via upload

---
+++ data_type_test_22.py
@@ -0,0 +1,27 @@
+nums = [2, 1, 5, 1, 3, 2]
+k = 3   
+b=0    
+for i in range(len(nums)-k+1):
+    s=0
+    for j in range(i,k+i):
+        s=s+nums[j]
+        if b<s:
+            b=s
+print(b)
+
+
+
+nums = [2, 1, 5, 1, 3, 2]
+K = 7
+
+l=0
+c=0
+max_l=0
+
+for i in range(len(nums)):
+    c+=nums[i]
+    while c>k:
+        c-=nums[l]
+        l+=1
+    max_l=max(max_l,i-l+1)
+print(max_l)
